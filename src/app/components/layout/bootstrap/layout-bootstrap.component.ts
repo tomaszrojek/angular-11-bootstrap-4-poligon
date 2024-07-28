6cf2237854002c9c6ef0b710caca9b8629c1c0e7
tomaszrojek <tomasz.rojek.81@gmail.com>
2024-07-28 15:33:21 +0200

    4 files modified, 2 added, 2 deleted

--- src/app/components/layout/bootstrap/layout-bootstrap.component.ts
+++ src/app/components/layout/bootstrap/layout-bootstrap.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout-bootstrap',
   templateUrl: './layout-bootstrap.component.html',
-  styleUrls: ['./layout-bootstrap.component.css'],
+  styleUrls: ['./layout-bootstrap.component.scss'],
 })
 export class LayoutBootstrapComponent implements OnInit {
   constructor() {}
--- src/app/components/layout/css/layout-css.component.ts
+++ src/app/components/layout/css/layout-css.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout-css',
   templateUrl: './layout-css.component.html',
-  styleUrls: ['./layout-css.component.css'],
+  styleUrls: ['./layout-css.component.scss'],
 })
 export class LayoutCssComponent implements OnInit {
   constructor() {}
